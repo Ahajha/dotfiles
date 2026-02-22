67e35ae21fd051f168edfc21f0bad1b1ad47b615
Alex Trotta <ahajha@gmail.com>
2026-02-22 02:53:52 -0500

    Add vscode config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,9 @@
+rm ~/.gitconfig
 ln -s $PWD/gitconfig ~/.gitconfig
+
+# A bit manual for now but this should be okay
+mkdir -p ~/.config/Code/User
+rm ~/.config/Code/User/keybindings.json
+rm ~/.config/Code/User/settings.json
+ln -s $PWD/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s $PWD/vscode/settings.json ~/.config/Code/User/settings.json
