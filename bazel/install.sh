ce7e7158773db683f9f9c7a3ccd2a391818e78c7
Alex Trotta <ahajha@gmail.com>
2026-02-22 18:07:50 -0500

    add buildifier

--- bazel/install.sh
+++ bazel/install.sh
@@ -6,14 +6,17 @@ mkdir -p bin
 
 # bazelisk
 readonly bazelisk_version=1.28.1
+readonly buildifier_version=8.5.1
 
 if [[ "$(uname -ms)" == "Linux x86_64" ]]; then
-    readonly bazelisk_arch="linux-amd64"
+    readonly arch="linux-amd64"
 else
     echo "Unknown arch: $(uname -ms)"
     exit 1
 fi
 
-curl -L --output $PWD/bin/bazel "https://github.com/bazelbuild/bazelisk/releases/download/v${bazelisk_version}/bazelisk-${bazelisk_arch}"
+curl -L --output $PWD/bin/bazel "https://github.com/bazelbuild/bazelisk/releases/download/v${bazelisk_version}/bazelisk-${arch}"
+curl -L --output $PWD/bin/buildifier "https://github.com/bazelbuild/buildtools/releases/download/v${buildifier_version}/buildifier-${arch}"
 
 chmod +x $PWD/bin/bazel
+chmod +x $PWD/bin/buildifier
