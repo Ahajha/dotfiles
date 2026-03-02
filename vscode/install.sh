3f27c26aa0dc5a0ef3b16adce554642d65ff1504
Alex Trotta <atrotta@modular.com>
2026-03-02 11:24:49 -0500

    force remove vscode files
    in case they don't exist

--- vscode/install.sh
+++ vscode/install.sh
@@ -24,7 +24,7 @@ code --install-extension $PWD/downloads/bazelrc-lsp.vsix
 
 # A bit manual for now but this should be okay
 mkdir -p ~/.config/Code/User
-rm ~/.config/Code/User/keybindings.json
-rm ~/.config/Code/User/settings.json
+rm -f ~/.config/Code/User/keybindings.json
+rm -f ~/.config/Code/User/settings.json
 ln -s $PWD/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 ln -s $PWD/vscode/settings.json ~/.config/Code/User/settings.json
