7453d945ab149f4287ae2ee6f477228d23356da1
Alex Trotta <ahajha@gmail.com>
2026-03-24 02:01:31 -0400

    add ty, remove other unnecessary python extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,12 +3,11 @@
 set -euo pipefail
 
 extensions=(
+astral-sh.ty
 bazelbuild.vscode-bazel
 charliermarsh.ruff
 llvm-vs-code-extensions.vscode-clangd
 ms-python.python
-ms-python.vscode-pylance
-ms-python.vscode-python-envs
 ms-vscode.cpptools
 rvben.rumdl
 )
